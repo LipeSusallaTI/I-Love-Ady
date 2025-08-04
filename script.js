ad834c47805cef7ac681eab7007f0cd92206886f
LipeSusallaTI <lipesusallati@gmail.com>
2025-08-04 03:00:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function verificarSenha() {
+  const senha = document.getElementById("senha").value;
+  const senhaCorreta = "oi"; // <- você pode trocar essa senha
+
+  if (senha === senhaCorreta) {
+    document.getElementById("envelope").classList.add("open");
+    setTimeout(() => {
+      window.location.href = "outra-pagina.html";
+    }, 1500);
+  } else {
+    document.querySelector("P").innerText = "Senha incorreta! Tenta de novo 😉";
+  }
+}
